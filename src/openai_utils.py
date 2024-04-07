e92ef260ca8ea1dcecfb191280a1ad5dcafe80e3
JacekKorta <jacek.korta@gmail.com>
2024-04-07 20:42:31 +0200

    Add generic function for openAi chat.completions call

--- src/openai_utils.py
+++ src/openai_utils.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import Any, Dict, List
 
 from openai import OpenAI
 
@@ -9,5 +9,20 @@ client: OpenAI = OpenAI(api_key=settings.openai_api_key)
 
 
 def is_flagged(data: List[str]) -> bool:
+    # todo: refactor, add dependency injection for client
     response = client.moderations.create(input=data)
     return any(m.flagged for m in response.results)
+
+
+def ask_model(
+    client: OpenAI,
+    messages: List[Dict[str, Any]],
+    model: str = "gpt-3.5-turbo",
+    temperature: float = 0.3,
+) -> str:
+    print(f"Make request to model, with messages = {messages}")
+    response = client.chat.completions.create(
+        temperature=temperature, model=model, messages=messages
+    )  # todo: zamień na logi
+    print(f"Model response {response.choices[0].message.content}")
+    return response.choices[0].message.content
